dd519ca2022f45104f18b5cbc150d6878f7c04b8
Nicholas Fletcher <njfletcher21@gmail.com>
2021-01-13 22:13:18 -0600

    added Neuroph class

---
+++ src/main/java/com/StockPrediction/NeurophModel.java
@@ -0,0 +1,18 @@
+package com.StockPrediction;
+
+import org.neuroph.core.data.DataSet;
+
+public class NeurophModel {
+
+    private int inpSize =2;
+    private int outSize =1;
+
+    public void inputFile(){
+
+        DataSet set = new DataSet(inpSize, outSize);
+
+
+
+
+    }
+}
